b5ead5290c6478ee300738dc70244c4603427f60
Tania <tania.rojas@edu.itescia.fr>
2019-12-16 11:19:50 +0100

    Houses

--- src/com/company/Houses.java
+++ src/com/company/Houses.java
@@ -10,4 +10,35 @@ public class House {
 - le prix (entier)
      */
 
+    private String title;
+    private String ref;
+    private String category;
+    private int price;
+
+    public House(String title, String ref, String category, int price) {
+        this.title = title;
+        this.ref = ref;
+        this.category = category;
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getRef() {
+        return ref;
+    }
 }
